5c4ce557e0466f2a047ce8ea43e3632dc5b5f454
xdenotte <73490483+xdenotte@users.noreply.github.com>
2025-08-01 01:55:26 +0200

    Update hestia.nix

--- hosts/hestia.nix
+++ hosts/hestia.nix
@@ -1,7 +1,7 @@
 
 { config, pkgs, ... }: {
   imports = [ ./../modules/system.nix ];
-  ./hosts/hardware-configuration.nix
+  ./../hosts/hardware-configuration.nix
 
   networking.hostName = "hestia";
 
